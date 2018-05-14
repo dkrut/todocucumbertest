1e25eb3b40e986e66076a4e6e0c3cf17b6a58a76
dkrut <dd.krutikov@gmail.com>
2018-05-14 22:39:20 +0300

    created tests for action_todo.feature

--- src/test/java/org/bitbucket/dkrut/features/action_todo.feature
+++ src/test/java/org/bitbucket/dkrut/features/action_todo.feature
@@ -10,3 +10,13 @@ Scenario: Unmark todo
     When user click checkbox at 1 todo
     And click checkbox at 1 todo else
     Then checkbox at 1 todo is unselected
+
+Scenario: Edit todo
+    Given user create 1 new todo
+    When user make double click at 1 line todo
+    Then 1 line todo not be a readonly
+
+Scenario: Show delete button
+    Given user create 1 new todo
+    When user hover to 1 line todo
+    Then delete button at 1 line todo is visible
--- src/test/java/org/bitbucket/dkrut/steps/ActionTodoSteps.java
+++ src/test/java/org/bitbucket/dkrut/steps/ActionTodoSteps.java
@@ -1,12 +1,11 @@
 package org.bitbucket.dkrut.steps;
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.bitbucket.dkrut.pages.TodoMVC;
 
-import static com.codeborne.selenide.Condition.selected;
+import static com.codeborne.selenide.Condition.*;
 
 /**
  * Created by Denis Krutikov on 05.05.2018.
@@ -30,6 +29,16 @@ public class ActionTodoSteps {
         todo.clickCheckboxTodo(count);
     }
 
+    @When("^user make double click at (\\d+) line todo$")
+    public void user_make_double_click_at_line_todo(int lineTodo) {
+        todo.lineTodo(lineTodo).doubleClick();
+    }
+
+    @When("^user hover to (\\d+) line todo$")
+    public void user_hover_to_line_todo(int linetodo) {
+        todo.lineTodo(linetodo).hover();
+    }
+
     @Then("^checkbox at (\\d+) todo is selected$")
     public void checkbox_is_selected(int count) {
         todo.checkboxTodo(count).shouldBe(selected);
@@ -38,5 +47,15 @@ public class ActionTodoSteps {
     @Then("^checkbox at (\\d+) todo is unselected$")
     public void checkbox_is_unselected(int count) {
         todo.checkboxTodo(count).shouldNotBe(selected);
+    }
+
+    @Then("^(\\d+) line todo not be a readonly$")
+    public void line_todo_status(int lineTodo) {
+        todo.lineTodo(lineTodo).shouldNotBe(readonly);
+    }
+
+    @Then("^delete button at (\\d+) line todo is visible$")
+    public void delete_button_status(int lineTodo){
+        todo.deleteButton(lineTodo).shouldBe(visible);
     }
 }
